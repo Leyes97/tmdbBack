1eb318b42548857af19ad3a8015c7756d96000c9
francisco <francisco.leyescampoy@gmail.com>
2024-08-20 19:57:10 -0300

    git ignore fixed

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -2,9 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 //controllers
-const { allGenres, cinemaMovies } = require('../controllers/moviesController');
+const {
+  allGenres,
+  cinemaMovies,
+  popularMovies,
+} = require('../controllers/moviesController');
 
 router.get('/genres', allGenres); // trae todos los generos de peliculas disponibles;
 router.get('/cinema', cinemaMovies); // trae todas las eliculas actualmente en el cine;
+router.get('/popular', popularMovies); // trae todas las peliculas populares
 
 module.exports = router;
