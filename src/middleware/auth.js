46b54f922bcf1d46ea27bfccef486a58c1ce9c9e
francisco <francisco.leyescampoy@gmail.com>
2024-08-22 22:08:16 -0300

    se arreglo el error de cors y manejo de las cookies

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,15 +1,20 @@
-const { validateToken } = require("../config/tokens");
+const { validateToken } = require('../config/tokens');
 
 function validateAuth(req, res, next) {
-  const token = req.cookies.token;
-  if (!token) return res.sendStatus(401);
+  try {
+    const token = req.cookies.token;
+    if (!token) return res.sendStatus(401);
 
-  const { user } = validateToken(token);
-  if (!user) return res.sendStatus(401);
+    const { user } = validateToken(token);
+    if (!user) return res.sendStatus(401);
 
-  req.user = user;
+    req.user = user;
 
-  next();
+    next();
+  } catch (error) {
+    console.log('error en la validacion', error);
+    res.sendStatus(500);
+  }
 }
 
 module.exports = { validateAuth };
