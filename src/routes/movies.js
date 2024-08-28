603ae082fffe3e9da6ce13e2386941c2322e4f6a
francisco <francisco.leyescampoy@gmail.com>
2024-08-28 10:00:45 -0300

    se creo el endpoint de traer peliculas por id y sus videos

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -9,6 +9,7 @@ const {
   topMovies,
   upcomingMovies,
   searchMovie,
+  getMovieById,
 } = require('../controllers/moviesController');
 
 router.get('/genres', allGenres); // trae todos los generos de peliculas disponibles;
@@ -17,5 +18,5 @@ router.get('/popular', popularMovies); // trae todas las peliculas populares
 router.get('/top', topMovies); // trae todas las peliculas mejor valoradas
 router.get('/upcoming', upcomingMovies); // trae todas las proximas peliculas a estrenar
 router.get('/search', searchMovie); //busca peliculas por nombre
-
+router.get('/:id', getMovieById); // busca peliculas por id
 module.exports = router;
